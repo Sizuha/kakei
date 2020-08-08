d5330cd37c06e7a56af1407b3bcfe0b3e6b554ec
Sizuha <sizuha.lily@gmail.com>
2020-08-08 14:38:04 +0900

    remake project

---
+++ RakuKakei/UI/Main/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  RakuKakei
+//
+//  Created by ILKYOUNG HWANG on 2020/08/08.
+//  Copyright © 2020 Sizuha. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "楽家計"
+
+        
+    }
+
+
+
+}
